ef6dfb1d74b24721f1623779aa2018fd23ac2dc9
VidyaSagarMehar <vidyasagark890@gmail.com>
2023-07-25 16:06:56 +0530

    add multiple state variable in class based component

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,13 @@
+import { useState } from 'react';
+
 const User = ({ name }) => {
+	const [count] = useState(0);
+	const [count2] = useState(1);
+
 	return (
 		<div className="user-card">
+			<h1>count: {count}</h1>
+			<h1>count 2: {count2}</h1>
 			<h2>Name: {name}</h2>
 			<h3>Location: Bangalore</h3>
 			<h4>Contact: vidyasagark890@gmail.com</h4>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,14 +4,23 @@ class UserClass extends React.Component {
 	// props in class based component
 	constructor(props) {
 		super(props);
-		console.log(props);
+
+		// Creating state variable on the class based component
+		this.state = {
+			count: 0,
+			// second state variable in class componet
+			count2: 2,
+		};
 	}
 
 	render() {
 		// destructuring the props
 		const { name, location } = this.props;
+		const { count, count2 } = this.state;
 		return (
 			<div className="user-card">
+				<h1>count: {count}</h1>
+				<h1>count 2: {count2}</h1>
 				<h2>Name: {name}</h2>
 				<h3>Location: {location}</h3>
 				<h4>Contact: vidyasagark890@gmail.com</h4>
