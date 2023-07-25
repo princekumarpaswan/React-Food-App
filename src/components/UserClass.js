79010294021ed2651d2b0ce307565af28a9e638a
VidyaSagarMehar <vidyasagark890@gmail.com>
2023-07-25 23:31:43 +0530

    Github users api added

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -7,32 +7,30 @@ class UserClass extends React.Component {
 
 		// Creating state variable on the class based component
 		this.state = {
-			count: 0,
-			// second state variable in class componet
-			count2: 2,
+			userInfo: {
+				name: 'Dummy',
+				location: 'Data',
+			},
 		};
 	}
 
+	async componentDidMount() {
+		const data = await fetch('https://api.github.com/users/VidyaSagarMehar');
+		const json = await data.json();
+
+		this.setState({
+			userInfo: json,
+		});
+
+		console.log(json);
+	}
+
 	render() {
-		// destructuring the props
-		const { name, location } = this.props;
-		const { count, count2 } = this.state;
+		// destructuring
+		const { name, location, avatar_url } = this.state.userInfo;
 		return (
 			<div className="user-card">
-				<h1>count: {count}</h1>
-				<button
-					onClick={() => {
-						// NEVER UPDATE THE VARIABLE DIRECLTY
-						// Right way below
-						this.setState({
-							count: this.state.count + 1,
-							count2: this.state.count2 + 1,
-						});
-					}}
-				>
-					+
-				</button>
-				<h1>count 2: {count2}</h1>
+				<img src={avatar_url} />
 				<h2>Name: {name}</h2>
 				<h3>Location: {location}</h3>
 				<h4>Contact: vidyasagark890@gmail.com</h4>
