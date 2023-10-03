3fb3a353fd6e4d3f21899625b24397486287e703
princekumarpaswan <prasantandal123@gmail.com>
2023-10-03 11:30:45 +0530

    added new file of testting and all testing passed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,7 @@ const Contact = () => {
 	return (
 		<div>
 			<h1>This is contact page</h1>
+			<button>click me</button>
 		</div>
 	);
 };
--- src/components/__tests__/Contect.test.js
+++ src/components/__tests__/Contect.test.js
@@ -6,8 +6,24 @@ test("Should load contact us component", () => {
 
     render(<Contact />)
 
-    const heading = screen.getAllByRole("heading");
+    const heading = screen.getByRole("heading");
 
-    expect(heading)
+    expect(heading).toBeInTheDocument();
 
+})
+
+test("Should load contact us component button element", () => {
+    render(<Contact />)
+
+    const button = screen.getByRole("button");
+
+    expect(button).toBeInTheDocument();
+})
+
+test("Should load contact us component button element text", () => {
+    render(<Contact />)
+
+    const button = screen.getByText("click me");
+
+    expect(button).toBeInTheDocument();
 })
